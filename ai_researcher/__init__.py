a3066d9ee002b6f159222006d3555a1c22068a34
Aayam Bansal <aayambansal@gmail.com>
2025-07-06 00:24:18 +0530

    Update __init__.py

--- ai_researcher/__init__.py
+++ ai_researcher/__init__.py
@@ -1,8 +1,15 @@
 from .cycle_researcher import CycleResearcher
 from .cycle_reviewer import CycleReviewer
 from .deep_reviewer import DeepReviewer
+from .evidence_reviewer import EvidenceReviewer
 from .detector import AIDetector
 
-__all__ = ['CycleResearcher', 'CycleReviewer', 'DeepReviewer', 'AIDetector']
+__all__ = [
+    'CycleResearcher',
+    'CycleReviewer',
+    'DeepReviewer',
+    'EvidenceReviewer',
+    'AIDetector',
+]
 
 __version__ = '0.1.0'
